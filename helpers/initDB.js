35aebca161f37acf126447f02269f1718980645c
Ratnadeep Das Choudhury <ratnadeepastronomy@gmail.com>
2021-05-14 01:56:20 +0530

    mongo connected

--- helpers/initDB.js
+++ helpers/initDB.js
@@ -1,5 +1,15 @@
 import mongoose from 'mongoose'
 
 function initDB(){
-    
+ mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true
+ })
+ mongoose.connection.on('connected',()=>{
+     console.log("connected to Mongo")
+ })
+ mongoose.connection.on('error',(err)=>{
+     console.log("error:",err)
+ })
 }
+
+export default initDB
