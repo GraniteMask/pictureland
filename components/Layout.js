500f3e652d06bbcc4ffef60337aaa2f5c38e9566
Ratnadeep Das Choudhury <ratnadeepastronomy@gmail.com>
2021-05-14 15:38:39 +0530

    fetched from mongo code

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ const layout = ({children}) =>{
         <>
             <Head>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+                <link rel="stylesheet" href="/style.css" />
                 
             </Head>
             <NavBar />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,43 @@
 import Link from 'next/Link'
 import NavBar from '../components/Navbar'
 
-const Home = () =>{
+
+
+const Home = ({products}) =>{
+
+  const productList = products.map(product =>{
+    return(
+      <div className="card" key={product._id}>
+        <div className="card-image">
+          <img src={product.mediaUrl}/>
+          <span className="card-title">{product.name}</span>
+        </div>
+        <div className="card-content">
+          <p>Rs. {product.price}.</p>
+        </div>
+        <div className="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    )
+  })
+
+  console.log(products)
   return(
-  <div>
-    <h1>Hi</h1>
+  <div className="rootcard">
+    {productList}
   </div>
   )
+}
+
+export async function getStaticProps(){
+  const res = await fetch('http://localhost:3000/api/products')
+  const data = await res.json()
+  return{
+    props:{
+      products:data
+    }
+  }
 }
 
 export default Home
