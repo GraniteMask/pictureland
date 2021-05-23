bd4e61da45f5e12f706fb31efd94edffd4641270
Ratnadeep Das Choudhury <ratnadeepastronomy@gmail.com>
2021-05-23 23:34:45 +0530

    signup page ui added

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = ({products}) =>{
 
   const productList = products.map(product =>{
     return(
-      <div className="card" key={product._id}>
+      <div className="card pcard" key={product._id}>
         <div className="card-image">
           <img src={product.mediaUrl}/>
           <span className="card-title">{product.name}</span>
--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,19 @@
 import Link from 'next/Link'
+import { useState } from 'react'
 
 const Signup = () =>{
+    const [name,setName] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
     return(
-        <div>
-            <h1>Signup Page</h1>
+        <div className="container authcard center-align">
+            <h3>SignUp</h3>
+            <input type="text" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)}/>
+            <input type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            <button className="btn waves-effect waves-light #5e35b1 deep-purple darken-1" type="submit" name="action">Signup
+                <i className="material-icons right">forward</i>
+            </button>
         </div>
     )
 }
