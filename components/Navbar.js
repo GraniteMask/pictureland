5b1ce4d058769ea16d068e18515255c3bddc80a7
Ratnadeep Das Choudhury <ratnadeepastronomy@gmail.com>
2021-07-08 00:52:18 +0530

    nookies added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,18 @@
 import Link from 'next/link'
 import {useRouter} from 'next/router'
+import {parseCookies} from 'nookies'
 
 const NavBar = () =>{
     const router = useRouter()
+    const {token} = parseCookies()
+    let user = false
+
+    if(token){
+        user = true
+    }else{
+        user = false
+    }
+
     function isActive(route){
         if(route == router.pathname){
             return "active"
@@ -14,9 +24,24 @@ const NavBar = () =>{
             <div className="nav-wrapper #5e35b1 deep-purple darken-1">
             <Link href="/"><a className="brand-logo left">Logo</a></Link>
             <ul id="nav-mobile" className="right">
-                <li className={isActive('/login')}><Link href="/login" ><a >Login</a></Link></li>
-                <li className={isActive('/signup')}><Link href="/signup" ><a >Signup</a></Link></li>
-                <li className={isActive('/create')}><Link href="/create" ><a >create</a></Link></li>
+                {
+                    user && 
+                    <li className={isActive('/create')}><Link href="/create" ><a >create</a></Link></li>
+                }
+                {
+                    user ?
+                    <>
+                    <li className={isActive('/login')}><Link href="/login" ><a >Account</a></Link></li>
+                    <li ><button className="btn">Logout</button></li>
+                    </>
+                    :
+                    <>
+                    <li className={isActive('/login')}><Link href="/login" ><a >Login</a></Link></li>
+                    <li className={isActive('/signup')}><Link href="/signup" ><a >Signup</a></Link></li>
+                    </>
+                }
+                
+                
             </ul>
             </div>
         </nav>
