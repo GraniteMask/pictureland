9249241cb64927d8741a62779bad08c248a86529
Ratnadeep Das Choudhury <ratnadeepastronomy@gmail.com>
2021-07-08 01:16:10 +0530

    account page redirect if not login

--- pages/account.js
+++ pages/account.js
@@ -1,6 +1,20 @@
+import {parseCookies} from 'cookies'
+
 const Account = () =>{
     return(
         <h1>Profile Page</h1>)
+}
+
+export async function getServerSideprops(ctx){
+   const {token} = parseCookies(ctx)
+   if(!token){
+       const {res} = ctx
+       res.writeHead(302,{Location:"/login"}) //to redirect is user isn't logged/not have token
+       res.end()
+   }
+   return{
+       props: {}
+   }
 }
 
 export default Account
