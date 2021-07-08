5dcd5e2195135d121ab297caf9807911297a86d0
Ratnadeep Das Choudhury <ratnadeepastronomy@gmail.com>
2021-07-09 00:42:19 +0530

    cart schema created

---
+++ models/Cart.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const {ObjectId} = mongoose.Schema.Types
+
+const cartSchema = new mongoose.Schema({
+    user: {
+        type:ObjectId,
+        ref:"User"
+    },
+    products:[
+        {quantity:{type:Number,default:1},
+        product:{type:ObjectId,ref:"product"}
+    }
+    ]
+})
+
+export default mongoose.models.Cart || mongoose.model("Cart",cartSchema)
