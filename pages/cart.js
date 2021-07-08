98c13285547facbc2d98fb1a6ba23181a7f98e19
Ratnadeep Das Choudhury <ratnadeepastronomy@gmail.com>
2021-07-09 01:23:22 +0530

    cart error handling added

--- pages/cart.js
+++ pages/cart.js
@@ -1,6 +1,15 @@
 import baseUrl from '../helpers/baseUrl'
 import {parseCookies} from 'nookies'
-const Cart = () =>{
+import cookie from 'js-cookie'
+import {useRouter} from 'next/router'
+const Cart = ({error}) =>{
+    const router = useRouter()
+    if(error){
+        M.toast({html:error,classes:"red"})
+        cookie.remove("user")
+        cookie.remove("token")
+        router.push('/login')
+    }
     return(
         <h1>cart page</h1>
         )
@@ -20,6 +29,11 @@ export async function getServerSideProps(ctx){
         }
     })
     const products = await res.json()
+    if(products.error){
+        return{
+           props:{error:products.error} 
+        }
+    }
     console.log(products)
     return {
         props:{products}
