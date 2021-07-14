5767aa13be277eac41e2ef570975b3015257cb7c
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-15 01:28:30 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,16 @@ const Home = ({products}) =>{
   )
 }
 
-export async function getStaticProps(){
+// export async function getStaticProps(){
+//   const res = await fetch(`${baseUrl}/api/products`)
+//   const data = await res.json()
+//   return{
+//     props:{
+//       products:data
+//     }
+//   }
+// }
+export async function getServerSideProps(){
   const res = await fetch(`${baseUrl}/api/products`)
   const data = await res.json()
   return{
