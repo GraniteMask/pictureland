531d6ae4dfa539bb586217c6d5b750153ddb6870
Ratnadeep Das Choudhury <ratnadeepastronomy@gmail.com>
2021-07-14 14:24:41 +0530

    orders in account page

--- pages/account.js
+++ pages/account.js
@@ -1,18 +1,49 @@
 import {parseCookies} from 'nookies'
 import baseUrl from '../helpers/baseUrl'
+import { useEffect, useRef } from 'react'
 
-const Account = () =>{
+const Account = ({orders}) =>{
+    const orderCard = useRef(null)
     const cookie = parseCookies()
     const user = cookie.user ? JSON.parse(cookie.user) : ""
+
+    useEffect(()=>{
+        M.Collapsible.init(orderCard.current)
+    },[])
+
+    const OrderHistory = () =>{
+        return(
+            <ul class="collapsible" ref={orderCard}>
+                {orders.map(item=>{
+                    return(
+                        <li>
+                            <div className="collapsible-header"><i className="material-icons">folder</i>{item.createdAt}</div>
+                            <div className="collapsible-body">
+                                <h5>Total Rs. {item.total}</h5>
+                                {
+                                    item.products.map(pitem=>{
+                                        return <h6>{pitem.product.name} x {pitem.quantity}</h6>
+                                    })
+                                }
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>    
+        )
+    }
     return(
         // <h1>Profile Page</h1>
         <div className="container">
             <div className="center-align">
                 <h4>{user.name}</h4>
                 <h4>{user.email}</h4>
+                <OrderHistory />
             </div>
         </div>
         )
+
+    
 }
 
 export async function getServerSideProps(ctx){
@@ -32,7 +63,7 @@ export async function getServerSideProps(ctx){
    console.log(res2)
 
    return{
-       props: {}
+       props: {orders:res2}
    }
 }
 
