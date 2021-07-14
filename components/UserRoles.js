be2f2fe8ef715af6982cbd943c02f8723f07e268
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-15 00:06:44 +0530

    user api added

--- components/UserRoles.js
+++ components/UserRoles.js
@@ -36,7 +36,7 @@ function UserRoles(){
                     <tr>
                         <td>{item.name}</td>
                         <td>{item.email}</td>
-                        <td>{item.role}</td>
+                        <td onClick={()=>handleRole()}>{item.role}</td>
                     </tr>  
                 )
             })}
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,7 +1,31 @@
 import User from '../../models/User'
 import Authenticated from '../../helpers/Authenticated'
 
-export default Authenticated(async(req,res)=>{
+
+export default async (req,res)=>{
+    switch(req.method){
+        case "GET":
+            await fetchUsers(req,res)
+            break
+        case "PUT":
+            await changeRole(req,res)
+            break
+       
+    }
+}
+
+const fetchUsers = Authenticated(async(req,res)=>{
     const users = await User.find({_id:{$ne:req.userId}})
+    res.status(200).json(users)
+})
+
+const changeRole = Authenticated(async(req,res)=>{
+    const {_id,role} = req.body
+    const newRole = role == "user" ? "admin" :"user"
+    const users = await User.findOneAndUpdate(
+        {_id},
+        {role:newRole},
+        {new:true}
+    )
     res.status(200).json(users)
 })
