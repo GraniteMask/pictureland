03e350ca690ca9b78550348c547442fd8e8d01b9
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-14 21:11:13 +0530

    table for user roles added

--- components/UserRoles.js
+++ components/UserRoles.js
@@ -3,11 +3,12 @@ import {parseCookies} from 'nookies'
 import baseUrl from '../helpers/baseUrl'
 
 function UserRoles(){
+    const [users,setUsers] = useState([])
     const {token} = parseCookies();
     useEffect(()=>{
         fetchUser()
     },[])
-    const fetchUser = () =>{
+    const fetchUser = async() =>{
         const res = await fetch(`${baseUrl}/api/users`,{
             headers:{
                 "Authorization":token
@@ -15,9 +16,35 @@ function UserRoles(){
         })
         const res2 = await res.json()
         console.log(res2)
+        setUsers(res2)
     }
     return(
-        <h1>User Roles</h1>
+        <>
+        {/* <h1>User Roles</h1> */}
+        <table>
+        <thead>
+          <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+          </tr>
+        </thead>
+
+        <tbody>
+            {users.map(item=>{
+                return(
+                    <tr>
+                        <td>{item.name}</td>
+                        <td>{item.email}</td>
+                        <td>{item.role}</td>
+                    </tr>  
+                )
+            })}
+          
+        </tbody>
+      </table>
+            
+        </>
     )
 }
 
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,6 +1,7 @@
 import User from '../../models/User'
 import Authenticated from '../../helpers/Authenticated'
 
-export default Authenticated(aasync(req,res)=>{
-    const users = await
+export default Authenticated(async(req,res)=>{
+    const users = await User.find({_id:{$ne:req.userId}})
+    res.status(200).json(users)
 })
