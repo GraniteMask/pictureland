b5c6d9473a369cdef2a60636bd3ac9f0ae794ded
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-15 13:58:32 +0530

    final deploy

--- pages/account.js
+++ pages/account.js
@@ -3,7 +3,7 @@ import baseUrl from '../helpers/baseUrl'
 import { useEffect, useRef } from 'react'
 import UserRoles from '../components/UserRoles'
 
-const Account = ({orders}) =>{
+const Account = () =>{
     const orderCard = useRef(null)
     const cookie = parseCookies()
     const user = cookie.user ? JSON.parse(cookie.user) : ""
@@ -18,34 +18,34 @@ const Account = ({orders}) =>{
         M.Collapsible.init(orderCard.current)
     },[])
 
-    const OrderHistory = () =>{
-        return(
-            <ul class="collapsible" ref={orderCard}>
-                {orders.map(item=>{
-                    return(
-                        <li key={item._id}>
-                            <div className="collapsible-header"><i className="material-icons">folder</i>{item.createdAt}</div>
-                            <div className="collapsible-body">
-                                <h5>Total Rs. {item.total}</h5>
-                                {
-                                    item.products.map(pitem=>{
-                                        return <h6 key={pitem._id}>{pitem.product.name} x {pitem.quantity}</h6>
-                                    })
-                                }
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>    
-        )
-    }
+    // const OrderHistory = () =>{
+    //     return(
+    //         <ul class="collapsible" ref={orderCard}>
+    //             {orders.map(item=>{
+    //                 return(
+    //                     <li key={item._id}>
+    //                         <div className="collapsible-header"><i className="material-icons">folder</i>{item.createdAt}</div>
+    //                         <div className="collapsible-body">
+    //                             <h5>Total Rs. {item.total}</h5>
+    //                             {
+    //                                 item.products.map(pitem=>{
+    //                                     return <h6 key={pitem._id}>{pitem.product.name} x {pitem.quantity}</h6>
+    //                                 })
+    //                             }
+    //                         </div>
+    //                     </li>
+    //                 )
+    //             })}
+    //         </ul>    
+    //     )
+    // }
     return(
         // <h1>Profile Page</h1>
         <div className="container">
             <div className="center-align">
                 <h4>{user.name}</h4>
                 <h4>{user.email}</h4>
-                <h3>Order History</h3>
+                {/* <h3>Order History</h3>
                 {
                     orders.length == 0 ?
                     <div className="container center-align">
@@ -53,7 +53,7 @@ const Account = ({orders}) =>{
                     </div>
                     :
                     <OrderHistory />
-                }
+                } */}
                 {user.role == "root" && <UserRoles />} 
                 
             </div>
@@ -62,27 +62,86 @@ const Account = ({orders}) =>{
 
     
 }
+// const Account = ({orders}) =>{
+//     const orderCard = useRef(null)
+//     const cookie = parseCookies()
+//     const user = cookie.user ? JSON.parse(cookie.user) : ""
 
-export async function getServerSideProps(ctx){
-   const {token} = parseCookies(ctx)
-   if(!token){
-       const {res} = ctx
-       res.writeHead(302,{Location:"/login"}) //to redirect is user isn't logged/not have token
-       res.end()
-   }
+//     // if(orders.length == 0){
+//     //     return(
+            
+//     //     )
+//     // }
 
-   const res = await fetch(`${baseUrl}/api/orders`,{
-       headers:{
+//     useEffect(()=>{
+//         M.Collapsible.init(orderCard.current)
+//     },[])
+
+//     const OrderHistory = () =>{
+//         return(
+//             <ul class="collapsible" ref={orderCard}>
+//                 {orders.map(item=>{
+//                     return(
+//                         <li key={item._id}>
+//                             <div className="collapsible-header"><i className="material-icons">folder</i>{item.createdAt}</div>
+//                             <div className="collapsible-body">
+//                                 <h5>Total Rs. {item.total}</h5>
+//                                 {
+//                                     item.products.map(pitem=>{
+//                                         return <h6 key={pitem._id}>{pitem.product.name} x {pitem.quantity}</h6>
+//                                     })
+//                                 }
+//                             </div>
+//                         </li>
+//                     )
+//                 })}
+//             </ul>    
+//         )
+//     }
+//     return(
+//         // <h1>Profile Page</h1>
+//         <div className="container">
+//             <div className="center-align">
+//                 <h4>{user.name}</h4>
+//                 <h4>{user.email}</h4>
+//                 <h3>Order History</h3>
+//                 {
+//                     orders.length == 0 ?
+//                     <div className="container center-align">
+//                         <h2>You have no order history</h2>
+//                     </div>
+//                     :
+//                     <OrderHistory />
+//                 }
+//                 {user.role == "root" && <UserRoles />} 
+                
+//             </div>
+//         </div>
+//         )
+
+    
+// }
+
+// export async function getServerSideProps(ctx){
+//    const {token} = parseCookies(ctx)
+//    if(!token){
+//        const {res} = ctx
+//        res.writeHead(302,{Location:"/login"}) //to redirect is user isn't logged/not have token
+//        res.end()
+//    }
+
+//    const res = await fetch(`${baseUrl}/api/orders`,{
+//        headers:{
            
-           "Authorization":token
-       }
-   })
-   const res2 =  await res.json()
-//    console.log(res2)
+//            "Authorization":token
+//        }
+//    })
+//    const res2 =  await res.json()
+// //    console.log(res2)
 
-   return{
-       props: {orders:res2}
-   }
-}
+//    return{
+//        props: {orders:res2}
+//    }
+// }
 
 export default Account
